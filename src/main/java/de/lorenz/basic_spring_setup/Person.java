e280a47c6cd9fccc8dd62111fc8b4a6b5dee1ba5
lorenzhohmann <hohmann.lorenz99@gmail.com>
2023-10-26 10:03:16 +0200

    Ininitial Commit

---
+++ src/main/java/de/lorenz/basic_spring_setup/Person.java
@@ -0,0 +1,32 @@
+package de.lorenz.basic_spring_setup;
+
+public class Person {
+
+	private String name;
+	private String phone;
+
+	Person() {
+	}
+
+	public Person(String name, String phone) {
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getPhone() {
+		return this.phone;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
